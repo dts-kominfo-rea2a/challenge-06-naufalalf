b78fa5c14b2537f50a93e39f98b28556cfd57a9e
naufalalfikri <naufalalfikri@gmail.com>
2022-06-14 06:41:18 +0700

    sudah dikerjakan tugas10

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,50 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallBack) => {
+  let arrResult = [];
+
+  fs.readFile(
+    file1,{
+      encoding : "utf-8"
+    },
+    (err, data) => {
+      if(err){
+        return fnCallBack(err,null);
+      }
+      const dataResult = JSON.parse(data);
+      arrResult.push(dataResult.message.split(" ")[1]);
+    }
+  );
+
+  fs.readFile(
+    file2,{
+      encoding : "utf-8"
+    },
+    (err, data) => {
+      if(err){
+        return fnCallBack(err,null);
+      }
+      const dataResult = JSON.parse(data);
+      arrResult.push(dataResult[0].message.split(" ")[1]);
+    }
+  );
+
+  fs.readFile(
+    file3,{
+      encoding : "utf-8"
+    },
+    (err, data) => {
+      if(err){
+        return fnCallBack(err,null);
+      }
+      const dataResult = JSON.parse(data);
+      arrResult.push(dataResult[0].data.message.split(" ")[1]);
+      fnCallBack(null, arrResult);
+    }
+  );
+
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
